eb5fa0b6bf372c6fe875a0434e7eeb1beab163eb
YeKyawPaing <wiz.ash.1331@gmail.com>
2017-09-07 02:28:59 +0800

    Add project files.

---
+++ MvcFox/Migrations/201709061136361_addfullname1.cs
@@ -0,0 +1,16 @@
+namespace MvcFox.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addfullname1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ MvcFox/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace MvcFox.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MvcFox.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "MvcFox.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(MvcFox.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
